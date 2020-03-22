d5d7158603cb56e6dcf72231bc7b46f0f88ad19c
Facco99 <facchin810@gmail.com>
2020-03-22 17:16:35 +0100

    creazione tutti i component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { GameslistComponent } from './gameslist/gameslist.component';
+import { GamedetailComponent } from './gamedetail/gamedetail.component';
+import { EditgameComponent } from './editgame/editgame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    GameslistComponent,
+    GamedetailComponent,
+    EditgameComponent
   ],
   imports: [
     BrowserModule
