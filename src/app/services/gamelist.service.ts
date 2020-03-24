80155d54229c1bb07d8a8b2f94289df716f28f35
Facco99 <facchin810@gmail.com>
2020-03-24 11:46:12 +0100

    Aggiunta gioco

--- src/app/services/gamelist.service.ts
+++ src/app/services/gamelist.service.ts
@@ -8,7 +8,8 @@ export class GamelistService {
 
     private gameListStored: GameItem[] = [
       {id: 1, nome:"Fifa 20",descrizione: "Gioco di calcio",genere: "Sport",rating: 4,prezzo:70,annoUscita:new Date(2019, 9)},
-      {id: 2, nome:"Fortnite",descrizione: "100vs game",genere: "Battleroyal",rating: 5,prezzo:0,annoUscita:new Date(2017, 9)}
+      {id: 2, nome:"Fortnite",descrizione: "100vs game",genere: "Battleroyal",rating: 5,prezzo:0,annoUscita:new Date(2017, 9)},
+      {id: 2, nome:"Gta V",descrizione: "Online e storia",genere: "Action",rating: 5,prezzo:70,annoUscita:new Date(2015, 9)}
     ];
 
   constructor() { }
