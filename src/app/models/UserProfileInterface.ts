ffc805a9d2c0c2e867534308f21c91f86c9df1f8
Facco99 <facchin810@gmail.com>
2020-04-06 09:23:19 +0200

    Aggiunto Interfaccia utente

---
+++ src/app/models/UserProfileInterface.ts
@@ -0,0 +1,10 @@
+export interface UserProfileItem{
+    email:string;
+    username:string;
+    name:string;
+    lastname:string;
+    bithday:Date;
+    birthcity:string;
+    sex:string;
+    newsletter:boolean;
+}
